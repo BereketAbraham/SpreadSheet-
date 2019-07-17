2925b0f9319aaa9e22ac912a5dddf6d6b95e65a9
BEREKET ABRAHAM <bereketmichael741@gmail.com>
2019-07-17 10:17:47 -0500

    Sprints #7-8

--- src/edu/mum/cs/cs525/spreadsheet/Director.java
+++ src/edu/mum/cs/cs525/spreadsheet/Director.java
@@ -1,62 +1,41 @@
 package edu.mum.cs.cs525.spreadsheet;
 
 public class Director {
+
 	private SpreadSheet spreadsheet;
 
 	public Director(SpreadSheet sps) {
 		spreadsheet = sps;
 	}
 
-	public static boolean isNumeric(String str) {
-		try {
-			Double.parseDouble(str);
-			return true;
-		} catch (NumberFormatException e) {
-			return false;
-		}
-	}
-
 	public SpreadSheet getSpreadsheet() {
 		return spreadsheet;
 	}
 
-	public Cell cell(int row, int col) // Get a reference to cell at (row,col)
-	{
+	public void writeInCellText(int r, int c, String text) {
+		if (text != null)
+			spreadsheet.cell(r, c).setContent(new TextContent(text));
+	}
+
+	// Get a reference to cell at (row, col)
+	public Cell cell(int row, int col) {
 		return spreadsheet.cell(row, col);
-	}
-
-	private void setCell(int row, int col, String string) {
-		if (isNumeric(string)) {
-			spreadsheet.cell(row, col).setContent(new NumberContent(Float.parseFloat(string)));
-		} else if (isNumericExpression(string)) {
-			spreadsheet.cell(row, col).setContent(new Expression(string));
-		} else {
-			spreadsheet.cell(row, col).setContent(new TextContent(string));
-		}
-	}
-
-	public static boolean isNumericExpression(String string) {
-		boolean isNumericExpression = false;
-		String[] operands = null;
-
-		if(string.contains("+") || string.contains("-") || string.contains("*") || string.contains("/")) {
-			operands = string.split("[-+*/]");
-			isNumericExpression = true;
-		}
-
-		if (isNumericExpression) {
-			for (String s : operands) {
-				if (!isNumeric(s)) {
-					return false;
-				}
-			}
-		}
-
-		return isNumericExpression;
 	}
 
 	public String contents() {
 		return spreadsheet.show();
+	}
+
+	private void writeInCellNumber(int i, int j, float f) {
+		// TODO Auto-generated method stub
+
+		spreadsheet.cell(i, j).setContent(new NumberContent(f));
+
+	}
+
+	// numerical expression sprint3
+	public void WriteInCellExpression(int r, int c, Operator obj) {
+		// spreadsheet.cell(r, c).setContent(new NumericOperation());
 	}
 
 	public String describe() {
@@ -67,11 +46,11 @@ public class Director {
 		return spreadsheet.examine();
 	}
 
-	public void buildSample() // Build sample data for development purpose
-	{
+	// Build sample data for development purpose
+	public void buildSample() {
 		setCell(1, 1, "Airfare:");
 
-		setCell(1, 2, "485.15");
+		setCell(1, 2, "485.15f");
 
 		setCell(1, 3, "");
 
@@ -100,11 +79,11 @@ public class Director {
 
 		setCell(5, 1, "Meals:");
 
-		setCell(5, 2, "115+2+3+4*5*6/3-7/8");
+		setCell(5, 2, "150");
 
 		setCell(5, 3, "");
 
-		setCell(5, 4, "All meals combined");
+		setCell(5, 4, "This is all our meals");
 
 		setCell(7, 1, "Sub-total:");
 
@@ -123,17 +102,31 @@ public class Director {
 			subtract.subContent(new Reference(spreadsheet.cell(i, 2)));
 		}
 
-		// multiplying values
+		// spreadsheet.cell(7, 3).setContent(subtract);
+		// cell(7, 3).setContent(subtract);
+
+		// muliplying values
 		Multiply multiply = new Multiply();
 		for (int i = 1; i <= 5; i++) {
 			multiply.multContent(new Reference(spreadsheet.cell(i, 2)));
 		}
 
-		setCell(8, 1, "Tax:"); // Tax factor label
+		// spreadsheet.cell(7, 4).setContent(multiply);
+		/// cell(7, 4).setContent(multiply);
 
-		setCell(8, 2, "0.15");
+		// writeInCellNumber(7, 2, spreadsheet.cell(1, 2).data());
 
-		setCell(9, 1, "Total:");
+		// reference goes here
+		writeInCellText(7, 4, "This is just a reference to [1, 2], to test the \"Reference\" class and mechanism");
+		Reference ref = new Reference(cell(1, 2));
+
+		// cell(7,2).setContent(ref);
+
+		writeInCellText(8, 1, "Tax:"); // Tax factor label
+
+		writeInCellNumber(8, 2, 0.15f);
+
+		writeInCellText(9, 1, "Total:");
 
 		// calculating total
 
@@ -148,16 +141,15 @@ public class Director {
 		spreadsheet.cell(9, 2).setContent(subtract2);
 		cell(9, 2).setContent(subtract2);
 
-		setCell(10, 1, "Partners: ");
+		writeInCellText(10, 1, "Partners: ");
 
-		setCell(10, 2, "4");
+		writeInCellNumber(10, 2, 4);
 
-		setCell(11, 1, "Months: ");
+		writeInCellText(11, 1, "Months: ");
 
-		setCell(11, 2, "12");
+		writeInCellNumber(11, 2, 12);
 
-		setCell(12, 1, "Installments:");
-
+		writeInCellText(12, 1, "Installments:");
 		Division division = new Division();
 		division.divideContent(new Reference(cell(9, 2)));
 		division.divideContent(new Reference(cell(10, 2)));
@@ -166,6 +158,54 @@ public class Director {
 		spreadsheet.cell(12, 2).setContent(division);
 		cell(12, 2).setContent(division);
 
+//		setCell(13, 1, "Meals: ");
+//		setCell(13, 2, "115+2+3+4*5*6/3-7/8");
+		setCell(5, 2, "115+2+3+4*5*6/3-7/8");
+		setCell(13, 1, "[7,2]*(1-[8,2])");
+
+	}
+
+	private void setCell(int row, int col, String string) {
+		if (isNumeric(string)) {
+			spreadsheet.cell(row, col).setContent(new NumberContent(Float.parseFloat(string)));
+		} else if (isNumericExpression(string)) {
+			spreadsheet.cell(row, col).setContent(new Expression(string, spreadsheet));
+		} else {
+			spreadsheet.cell(row, col).setContent(new TextContent(string));
+		}
+	}
+
+	public static boolean isNumericExpression(String string) {
+		boolean isNumericExpression = false;
+		String[] operands = null;
+
+		if (string.contains("+") || string.contains("-") || string.contains("*") || string.contains("/")
+				|| string.contains(")") || string.contains("(")) {
+			operands = string.split("[-+*/()]");
+			isNumericExpression = true;
+		}
+
+		if (isNumericExpression) {
+			for (String s : operands) {
+				if(s.trim().isEmpty()) {
+					continue;
+				}
+				if (!isNumeric(s) && !s.matches("\\[[0-9]+,[0-9]+\\]")) {
+					return false;
+				}
+			}
+		}
+
+		return isNumericExpression;
+	}
+
+	public static boolean isNumeric(String str) {
+		try {
+			Double.parseDouble(str);
+			return true;
+		} catch (NumberFormatException e) {
+			return false;
+		}
 	}
 
 }
--- src/edu/mum/cs/cs525/spreadsheet/Expression.java
+++ src/edu/mum/cs/cs525/spreadsheet/Expression.java
@@ -6,37 +6,34 @@ import java.util.Stack;
 
 public class Expression extends Operator {
 
-	List<Operator> mOperations = new ArrayList<Operator>();
-	List<String> myDataList = new ArrayList<String>();
 	private float value;
 
-	public Expression(String string) {
-		this.value = evaluate(string);
+	List<Operator> mOperations = new ArrayList<Operator>();
+	List<String> myDataList = new ArrayList<String>();
+
+	public List<Operator> getNumericOperation() {
+		return mOperations;
+	}
+
+	public Expression(String string, SpreadSheet spreadSheet) {
+		this.value = evaluate(string, spreadSheet);
 	}
 
 	public Expression() {
 
 	}
 
-	// Returns true if 'op2' has higher or same precedence as 'op1',
-	// otherwise returns false.
-	public static boolean hasPrecedence(char op1, char op2) {
-
-		return (op1 != '*' && op1 != '/') || (op2 != '+' && op2 != '-');
-	}
-
-	public List<Operator> getNumericOperation() {
-		return mOperations;
-	}
+	List<Content> ref = new ArrayList<Content>();
 
 	@Override
 	public String value() {
-
+		// TODO Auto-generated method stub
 		return Float.toString(value);
 	}
 
-	@Override
-	public String formula() {
+	//@Override
+	public String formula2() {
+		// TODO Auto-generated method stub
 
 		StringBuffer sBuffer = new StringBuffer("[");
 		for (String string : myDataList) {
@@ -64,28 +61,115 @@ public class Expression extends Operator {
 		return sBuffer.toString();
 	}
 
+
+	@Override
+	public String formula() {
+
+		// Stack for numbers: 'values'
+		Stack<String> values = new Stack<String>();
+
+		// Stack for Operators: 'ops'
+		Stack<String> ops = new Stack<String>();
+
+		for (String token : myDataList) {
+			// Current token is a whitespace, skip it
+			if (token.trim().isEmpty())
+				continue;
+
+
+			if (isNumeric(token) || token.matches("\\[[0-9]+,[0-9]+\\]")) {
+				values.push(token);
+			}
+
+			// Current token is an opening brace, push it to 'ops'
+			else if (token.trim().equals("(")) {
+				ops.push(token);
+			}
+
+			// Closing brace encountered, solve entire brace
+			else if (token.trim().equals(")"))
+			{
+				while (!ops.peek().trim().equals("("))
+					values.push("(" + createOperationString(ops.pop().trim().charAt(0), values.pop(), values.pop()) + ")");
+				ops.pop();
+			}
+
+			else if (token.trim().equals("+") || token.trim().equals("-") || token.trim().equals("*") || token.trim().equals("/")) {
+				// While top of 'ops' has same or greater precedence to current
+				// token, which is an operator. Apply operator on top of 'ops'
+				// to top two elements in values stack
+				while (!ops.empty() && hasPrecedence(token.trim().charAt(0), ops.peek().trim().charAt(0)))
+					values.push("(" + createOperationString(ops.pop().trim().charAt(0), values.pop(), values.pop()) + ")");
+
+				// Push current token to 'ops'.
+				ops.push(token);
+			}
+
+
+		}
+
+		while (!ops.empty())
+			values.push("(" + createOperationString(ops.pop().trim().charAt(0), values.pop(), values.pop())+ ")");
+
+		// Top of 'values' contains result, return it
+		return values.pop();
+
+	}
+
+	public String createOperationString(char op, String b, String a) {
+
+		switch (op) {
+			case '+':
+				return a + " + " + b;
+			case '-':
+				return a + " - " + b;
+			case '*':
+				return a + " * " + b;
+			case '/':
+				return a + " / " + b;
+		}
+		return "";
+	}
+
 	@Override
 	public float data() {
+		// TODO Auto-generated method stub
 		return value;
 	}
 
-	public float evaluate(String expression) {
+	public float evaluate(String expression, SpreadSheet spreadSheet) {
 
 		char[] tokens = expression.toCharArray();
 
-		for (Character character : tokens) {
-
-		}
 		// Stack for numbers: 'values'
 		Stack<Float> values = new Stack<Float>();
 
 		// Stack for Operators: 'ops'
 		Stack<Character> ops = new Stack<Character>();
 
-		for (int i = 0; i < tokens.length; i++) {
+		for (int i = 0; i < tokens.length;)  {
 			// Current token is a whitespace, skip it
 			if (tokens[i] == ' ')
 				continue;
+
+			if (tokens[i] == '[') {
+				int indexClosingBracket = expression.indexOf(']', i);
+				String cell = expression.substring(i + 1, indexClosingBracket);
+				//System.out.println(cell);
+				String[] cellCoordinate = cell.split(",");
+				int xCor = Integer.parseInt(cellCoordinate[0]);
+				int yCor = Integer.parseInt(cellCoordinate[1]);
+				Cell cellRef = spreadSheet.cell(xCor, yCor);
+				//System.out.println(cellRef.value());
+				if (cellRef.value() == null || cellRef.value().trim().isEmpty()) {
+					values.push(0f);
+				} else {
+					values.push(Float.parseFloat(cellRef.value()));
+				}
+				myDataList.add("[" + cell + "]");
+
+				i = indexClosingBracket + 1;
+			}
 
 			if (tokens[i] >= '0' && tokens[i] <= '9') {
 				StringBuilder sb = new StringBuilder();
@@ -96,7 +180,24 @@ public class Expression extends Operator {
 				}
 				values.push(Float.parseFloat(sb.toString()));
 				myDataList.add(sb.toString());
-			} else if (tokens[i] == '+' || tokens[i] == '-' || tokens[i] == '*' || tokens[i] == '/') {
+			}
+
+			// Current token is an opening brace, push it to 'ops'
+			else if (tokens[i] == '(') {
+				ops.push(tokens[i]);
+				myDataList.add(tokens[i] + "");
+			}
+
+			// Closing brace encountered, solve entire brace
+			else if (tokens[i] == ')')
+			{
+				myDataList.add(tokens[i] + "");
+				while (ops.peek() != '(')
+					values.push(calculate(ops.pop(), values.pop(), values.pop()));
+				ops.pop();
+			}
+
+			else if (tokens[i] == '+' || tokens[i] == '-' || tokens[i] == '*' || tokens[i] == '/') {
 				// While top of 'ops' has same or greater precedence to current
 				// token, which is an operator. Apply operator on top of 'ops'
 				// to top two elements in values stack
@@ -107,6 +208,8 @@ public class Expression extends Operator {
 				ops.push(tokens[i]);
 				myDataList.add(tokens[i] + "");
 			}
+
+			i++;
 		}
 
 		// Entire expression has been parsed at this point, apply remaining
@@ -116,6 +219,17 @@ public class Expression extends Operator {
 
 		// Top of 'values' contains result, return it
 		return values.pop();
+	}
+
+	// Returns true if 'op2' has higher or same precedence as 'op1',
+	// otherwise returns false.
+	public static boolean hasPrecedence(char op1, char op2) {
+		if (op2 == '(' || op2 == ')')
+			return false;
+		if ((op1 == '*' || op1 == '/') && (op2 == '+' || op2 == '-'))
+			return false;
+		else
+			return true;
 	}
 
 	// A utility method to apply an operator 'op' on operands 'a'
@@ -147,6 +261,15 @@ public class Expression extends Operator {
 	@Override
 	public float operate(float a, float b) {
 		return 0;
+	}
+
+	public static boolean isNumeric(String str) {
+		try {
+			Double.parseDouble(str);
+			return true;
+		} catch (NumberFormatException e) {
+			return false;
+		}
 	}
 
 }
